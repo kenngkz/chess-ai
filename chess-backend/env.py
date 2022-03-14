9c18434eeecfd99be4aeed308e0a461a742a6c6d
kenngkz <kenngkz@gmail.com>
2022-03-14 01:18:03 +0000

    Env checkpoint

--- chess-backend/env.py
+++ chess-backend/env.py
@@ -1,26 +1,46 @@
+from game import Game
+import utils
 
+import numpy as np
 
-class EnvSave:
+# class EnvSave:
     
-    def __init__(self):
-        pass
+#     def __init__(self):
+#         pass
     
-    def save_to_file(self, filename):
-        ''' Saves an EnvSave obj to a file '''
-        pass
+#     def save_to_file(self, filename):
+#         ''' Saves an EnvSave obj to a file '''
+#         pass
 
-    @classmethod
-    def load_file(cls, filename):
-        ''' Load an EnvSave obj from a save file '''
-        pass
+#     @classmethod
+#     def load_file(cls, filename):
+#         ''' Load an EnvSave obj from a save file '''
+#         pass
 
 class ChessEnv:
+    '''
+    Chess-v0
 
-    def __init__(self):
+    observation structure: list of len 70
+        - 64 entries specifying the piece index in each cell (order: row1col1, row1col2, ...)
+        - 4 entries specifying the castle status (whether castling is allowed for each castle type)
+            - order: black queenside, black queenside, white queenside, white kingside
+        - 2 entries specifying whether each side is under check (order: white, black)
+    '''
+
+    def __init__(self, opponent=None):
+        self.game = None
+        self.opponent = opponent
+
+    @classmethod  # do i need this? can use set_state instead
+    def load(self, filepath):
         pass
 
     def reset(self):
-        pass
+        self.game = Game()
+        self.game.all_legal_moves()
+        self.action_space = self.game.legal_moves
+        return tuple([*utils.dict_to_arr(self.game.board.position).flatten(), *list(self.game.castle_status.values()), *self.game.check_status.values()])
 
     def step(self, action):
         pass
