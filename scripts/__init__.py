3a3c9d207cfb18ae185652a53bc543a1672abc9d
kenngkz <kenngkz@gmail.com>
2023-08-19 12:11:37 +0100

    Set the working directory for all scripts

---
+++ scripts/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+# this is the directory name of this __init__ file
+SCRIPTS_DIRNAME = "scripts"
+if os.path.basename(os.getcwd()) == SCRIPTS_DIRNAME:
+    os.chdir("..")  # change to the project directory
