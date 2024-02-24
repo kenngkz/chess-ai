aaf346aa7b8b15ee610a663b300d63dc42297cc9
Kenneth Ng <kenneth@algomo.com>
2024-02-24 21:34:43 +0000

    Typo

--- src/neural_network/common/legal_move.py
+++ src/neural_network/common/legal_move.py
@@ -8,7 +8,7 @@ from src.transformations.move import index_to_move
 def pick_top_legal_move(fen: str, predictions: List[float]) -> chess.Move:
     board = chess.Board(fen)
     predictions_with_index = [(idx, conf) for idx, conf in enumerate(predictions)]
-    predictions_with_index.sort(key=lambda x: x[1], reverse=True)å
+    predictions_with_index.sort(key=lambda x: x[1], reverse=True)
     for idx, conf in predictions_with_index:
         move = index_to_move(idx)
         if board.is_legal(move):
