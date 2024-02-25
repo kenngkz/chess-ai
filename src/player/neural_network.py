50718431db9653d87a50dc2bacb429a9d6402edf
Kenneth Ng <kenneth@algomo.com>
2024-02-25 17:53:11 +0000

    Greedy+non-greedy player move selection

--- src/player/neural_network.py
+++ src/player/neural_network.py
@@ -3,22 +3,26 @@ from chess import Move
 from tensorflow.python.keras import Model
 from tensorflow.python.keras.models import load_model
 
-from src.neural_network.common.legal_move import pick_top_legal_move
+from src.neural_network.common.legal_move import pick_prob_top_legal_move, pick_top_legal_move
 from src.schema.player import Player
 from src.transformations.fen_to_obs import parse_fen
 
 
 class NNPlayer(Player):
 
-    def __init__(self, model: Model = None, ckpt_path: str = None) -> None:
+    def __init__(self, model: Model = None, ckpt_path: str = None, greedy: bool = False) -> None:
         if not model and not ckpt_path:
             raise ValueError("Either one of `model` or `ckpt_path` is required.")
         if model:
             self.model = model
         else:
             self.model = load_model(ckpt_path)
+        self.greedy = greedy
 
     def select_move(self, fen_board: str) -> Move:
         obs = np.array([parse_fen(fen_board)])
         predictions = self.model.predict(obs, verbose=0)
-        return pick_top_legal_move(fen_board, predictions[0])
+        if self.greedy:
+            return pick_top_legal_move(fen_board, predictions[0])
+        else:
+            return pick_prob_top_legal_move(fen_board, predictions[0])
